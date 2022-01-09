5c64a8c0503bae0e031aba1a7d485ed2c50fff11
KhalilThabet <69755000+KhalilThabet@users.noreply.github.com>
2022-01-09 17:47:20 +0100

    Added handleSearch Module

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./styles.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-var array="";
+var array = "";
 export default function App() {
   const [Array, setArray] = useState([]);
-  var handleInputChange=(event)=>{
-    var target=event.target;
-    var name=target.name;
-    var value=target.value;
-    setArray([...Array,array]);
-  }
+  const [Filtred, setFiltred] = useState([]);
+  var handleInputChange = (event) => {
+    var target = event.target;
+    var name = target.name;
+    var value = target.value;
+    setArray([...Array, array]);
+  };
+  var handleSearch = (event) => {
+    setFiltred(Array.filter((word) => word.includes(event.target.value)));
+  };
   return (
     <div>
       <div className="App">
@@ -17,12 +21,18 @@ export default function App() {
         <h2>Start editing to see some magic happen!</h2>
       </div>
       <div>
-        Test : <br/>
-          <input onChange={(event)=>{array=event.target.value}}/>
-          <input type="submit" onClick={handleInputChange}/>
+        Test : <br />
+        <input
+          onChange={(event) => {
+            array = event.target.value;
+          }}
+        />
+        <input type="submit" onClick={handleInputChange} />
       </div>
       <div>
-        {Array.map(name => <h2>{name}</h2>)}
+        {Filtred.map((name) => (
+          <h2>{name}</h2>
+        ))}
       </div>
     </div>
   );
